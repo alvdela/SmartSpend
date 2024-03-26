89c51646a2f30ac252dc2573cf92fc7f651bbde4
alvdela <alvarodlr002@gmail.com>
2024-03-27 00:40:33 +0100

    Cambios menores en las vistas

--- app/src/main/java/com/alvdela/smartspend/LoginActivity.kt
+++ app/src/main/java/com/alvdela/smartspend/LoginActivity.kt
@@ -1,6 +1,7 @@
 package com.alvdela.smartspend
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,4 +18,6 @@ class LoginActivity : AppCompatActivity() {
             insets
         }
     }
+
+    fun forgotPassword(view: View) {}
 }
--- app/src/main/java/com/alvdela/smartspend/MainParentsActivity.kt
+++ app/src/main/java/com/alvdela/smartspend/MainParentsActivity.kt
@@ -2,14 +2,35 @@ package com.alvdela.smartspend
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
 
 class MainParentsActivity : AppCompatActivity() {
+
+    private lateinit var drawer: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_parents)
 
+    }
+
+    private fun initToolBar() {
+        val toolbar: Toolbar = findViewById(R.id.toolbar_main)
+        setSupportActionBar(toolbar)
+
+        drawer = findViewById(R.id.main_parents)
+        val toggle = ActionBarDrawerToggle(
+            this, drawer, toolbar, R.string.bar_tittle,
+            R.string.navigation_drawer_close
+        )
+
+        drawer.addDrawerListener(toggle)
+
+        toggle.syncState()
     }
 }
